cb97f3faf377bcb56fa4fb555894e7bb7932f379
anth0nycodes <hoanganthony2207@gmail.com>
2025-05-14 19:53:15 -0400

    screw mounting

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,28 +2,16 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { useEffect } from "react";
-import { useState } from "react";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
-      enableSystem={true}
+      defaultTheme="light"
+      enableSystem={false}
       disableTransitionOnChange
       {...props}
     >
