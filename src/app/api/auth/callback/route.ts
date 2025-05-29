888c2f5ed08c77320ab2e72bb26e6b7df821505d
anth0nycodes <hoanganthony2207@gmail.com>
2025-05-29 02:36:55 -0400

    feat(supabase): users are inserted into users table after authenticating

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -12,6 +12,20 @@ export async function GET(request: Request) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      await supabase.from("users").upsert({
+        id: user.id,
+        name: user.user_metadata.full_name || user.user_metadata.name,
+        email: user.email,
+        avatar_url: user.user_metadata.avatar_url,
+        created_at: new Date().toISOString(),
+      });
+
+      console.dir(user, { depth: null });
+
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
